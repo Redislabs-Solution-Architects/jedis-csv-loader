6d7b56d978f47eeb5e45da4539fb16e7a884323e
JayDBC <jay.dastur@redis.com>
2026-04-14 12:18:48 -0400

    20160414 Added Functionality

--- src/main/java/com/jsd/jedis/AppPubSub.java
+++ src/main/java/com/jsd/jedis/AppPubSub.java
@@ -1,14 +1,13 @@
 package com.jsd.jedis;
 
-
 import java.util.HashSet;
+import java.util.Scanner;
 
 import redis.clients.jedis.JedisPooled;
 import redis.clients.jedis.JedisPubSub;
 
 public class AppPubSub {
 
- 
     HashSet<String> keys = new HashSet<>();
 
     String keyPrefix;
@@ -16,13 +15,17 @@ public class AppPubSub {
 
     int eventCount = 0;
 
+    JedisPooled jedisPooled;
+    JedisPubSub jedisPubSub;
+
+    boolean printKeyEvent = false;
 
     public AppPubSub(String configFile) throws Exception {
         RedisDataLoader dataLoader = new RedisDataLoader(configFile);
-        JedisPooled jedisPooled = dataLoader.getJedisPooled();
+        this.jedisPooled = dataLoader.getJedisPooled();
 
         // Subscriber definition
-        JedisPubSub jedisPubSub = new JedisPubSub() {
+        this.jedisPubSub = new JedisPubSub() {
             @Override
             public void onMessage(String channel, String message) {
                 System.out.println("Channel: " + channel + " | Message: " + message);
@@ -30,44 +33,44 @@ public class AppPubSub {
 
             @Override
             public void onPMessage(String pattern, String channel, String message) {
-                addEvent(channel);
-                //System.out.println("Pattern: " + pattern + " | Channel: " + channel + " | Message: " + message);
+                logEvent(channel);
+
+                if (printKeyEvent) {
+                    System.out.println("Pattern: " + pattern + " | Channel: " + channel + " | Message: " + message);
+                }
+
             }
         };
-
-        // Subscribe to keyspace changes on all keys
-        new Thread(() -> jedisPooled.psubscribe(jedisPubSub, "__keyspace@0__:*")).start();
     }
 
-    private void addEvent(String event) {
-        //System.out.println("[KeySpaceListener] Event " + event);
+    private void logEvent(String event) {
+        // System.out.println("[KeySpaceListener] Event " + event);
 
-        if(event.startsWith("__keyspace@0__:" + this.keyPrefix)) {
-            keys.add(event);
-            eventCount++;
-            lastKey = event.substring(15);
-            //System.out.println("[KeySpaceListener] Added Event " + keys.size() + " " + event);
-        }
+        keys.add(event);
+        eventCount++;
+        lastKey = event.substring(15);
     }
 
     public void trackKeys(int targetKeys, boolean continuousTracking) throws Exception {
         System.out.println("[KeySpaceListener] Tracking Keys");
         Thread t = new Thread() {
-            public void run()  {
-                int marker = targetKeys/10;
-                while(true) {
-                    if(keys.size() == targetKeys) {
+            public void run() {
+                int marker = targetKeys / 10;
+                while (true) {
+                    if (keys.size() == targetKeys) {
                         System.out.println("[KeySpaceListener] Detected " + keys.size() + " Keys in Redis");
                         System.out.println("[KeySpaceListener] Last Key Added: " + lastKey);
                         break;
-                    }
-                    else if(continuousTracking && (keys.size() >= marker)) {
-                        marker = marker + (targetKeys/10);
+                    } else if (continuousTracking && (keys.size() >= marker)) {
+                        marker = marker + (targetKeys / 10);
                         System.out.println("[KeySpaceListener] Detected " + keys.size() + " Keys in Redis");
-                        System.out.println("[KeySpaceListener] Last Key Added: " + lastKey);                     
-                    }  
+                        System.out.println("[KeySpaceListener] Last Key Added: " + lastKey);
+                    }
 
-                    try {Thread.sleep(1000l);} catch(Exception e) {}
+                    try {
+                        Thread.sleep(1000l);
+                    } catch (Exception e) {
+                    }
                 }
             }
         };
@@ -75,15 +78,52 @@ public class AppPubSub {
         t.start();
     }
 
-    public void setPrefix(String  keyPrefix) {
+    public void setPrefix(String keyPrefix) {
         this.keyPrefix = keyPrefix;
     }
 
+    public void subscribeChannel(String channelName) {
 
+        // Subscribe a specific channel
+        new Thread(() -> jedisPooled.subscribe(jedisPubSub, channelName)).start();
+    }
+
+    public void listenKeySpace(String pattern) {
+
+        // Subscribe to keyspace changes on all keys
+        new Thread(() -> jedisPooled.psubscribe(jedisPubSub, "__keyspace@0__:" + pattern)).start();
+    }
 
     public static void main(String[] args) throws Exception {
-        AppPubSub ps = new AppPubSub("./config.properties");
+
+        Scanner s = new Scanner(System.in);
+        String configFile = "./config.properties";
+
+        System.out.print("\nEnter the config file path (Defaults to ./config.properties): ");
+        String configFile1 = s.nextLine();
+
+        if (!"".equals(configFile1)) {
+            configFile = configFile1;
+        }
+
+        AppPubSub ps = new AppPubSub(configFile);
+
+        System.err.print("\nChoose Option:\n[1] Subscribe to Channel\n[2] Listen to Key Space\nSelect: ");
+
+        String option = s.nextLine();
+
+        if ("1".equalsIgnoreCase(option)) {
+            System.err.print("Channel Name: ");
+            String channelName = s.nextLine();
+            ps.subscribeChannel(channelName);
+
+        } else if ("2".equalsIgnoreCase(option)) {
+            System.err.print("Key Pattern (e.g. trades:*) :  ");
+            String pattern = s.nextLine();
+            ps.listenKeySpace(pattern);
+            ps.printKeyEvent = true;
+        }
+
         ps.setPrefix("NA");
     }
 }
-
